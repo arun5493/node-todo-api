8b9ad722bf03ad191fc6840d4658cbd6f1c220ab
arun5493 <arun5493@gmail.com>
2017-08-19 11:45:30 -0700

    Added tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+// cd ~/mongo/bin/
+// ./mongod --dbpath ~/mongo-data
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,28 @@ const request = require('supertest');
 const{app} = require('./../server.js');
 const{Todo} = require('./../models/todo.js');
 
+const todos = [{
+  text:'First test todo'
+}, {
+  text: 'Second test todo'
+}];
 
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
+});
+
+describe('GET /todos', () => {
+  it('Should get all the Todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
 });
 
 describe('POST /todos', () => {
@@ -25,7 +44,7 @@ describe('POST /todos', () => {
             return done(err);
           }
 
-          Todo.find().then((todos)=>{
+          Todo.find({text}).then((todos)=>{
             expect(todos[0].text).toBe(text);
             done();
           }).catch((err) => done(err));
@@ -43,11 +62,10 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((err)=>done(err));
       })
 
-  })
-
+  });
 });
