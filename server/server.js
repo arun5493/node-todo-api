4ba0433ab12080e8cbdd9fb057fb58027c85af7b
arun5493 <arun5493@gmail.com>
2017-08-19 19:58:21 -0700

    Add tests for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -38,16 +38,16 @@ app.get('/todos', (req,res)  => {
 app.get('/todos/:id', (req,res) => {
   var id = req.params.id;
   if(!ObjectID.isValid(id)) {
-    return res.status(404).send();
+    return res.status(404).send({message:'Sorry.Invalid ID'});
   }
   Todo.findById(id).then((todo) => {
     if(!todo){
-      res.status(404).send();
+      return res.status(404).send({message:'Not available'});
     }
     res.status(200).send({todo});
   }).catch((e) => {
-    res.status(400).send(e));
-  }
+    res.status(400).send(e);
+  })
 });
 
 app.listen(3000, () => {
